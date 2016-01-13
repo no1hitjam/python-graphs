771d0fd3aef4508baf115d98c1e677189f2ba632
jlango <langoj@onid.orst.edu>
2016-01-12 21:08:03 -0800

    add testing

--- tests.py
+++ tests.py
@@ -2,15 +2,29 @@ from main import Graph
 # from shortest_path import dfs
 import pygraphviz
 
-test_graph1 = Graph({'a', 'b', 'c', 'd'}, {('a', 'b'), ('c', 'd'), ('d', 'b')})
 
-
-def display(graph):
+def convert_to_pvg(graph):
     py_graph = pygraphviz.AGraph()
     for vertex in graph.vertices:
         py_graph.add_node(vertex)
-    for edge in graph.edges:
-        py_graph.add_edge(edge.v_from, edge.v_to)
+    #py_graph.add_edge('a', 'd')
+    for v_from, v_tos in graph.edges.iteritems():
+        for v_to in v_tos:
+            py_graph.add_edge(v_from, v_to.vertex)
     return py_graph
 
-display(test_graph1)
+
+def test(number, graph):
+    number = str(number)
+    pvg = convert_to_pvg(graph)
+    pvg.layout()
+    pvg.draw('test results/test' + number + '.png')
+    print('wrote test' + number + '.png')
+
+
+test('001', Graph({'a', 'b', 'c', 'd'}, {
+                    ('a', 'b'),
+                    ('c', 'd'),
+                    ('d', 'b'),
+                    ('c', 'a')}))
+
